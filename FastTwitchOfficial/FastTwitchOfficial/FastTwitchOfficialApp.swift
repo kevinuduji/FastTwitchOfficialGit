52db25d4e9cc3dd62ca5457f1463ffcb16fda7db
Kevin Uduji <118309007+kevinuduji@users.noreply.github.com>
2024-06-09 14:00:38 -0500

    Starting Files

---
+++ FastTwitchOfficial/FastTwitchOfficial/FastTwitchOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTwitchOfficialApp.swift
+//  FastTwitchOfficial
+//
+//  Created by Kevin Uduji on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FastTwitchOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
